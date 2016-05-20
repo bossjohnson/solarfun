2222db2476bdb2b0dac263380ea4af999ec07d71
Ryan Kane <Kane@Ryans-MacBook-Pro.local>
2016-05-20 12:55:06 -0600

    functionality on joke page

--- routes/index.js
+++ routes/index.js
@@ -6,37 +6,27 @@ var http = require('http');
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', {
-        title: 'Express'
+        title: 'YoMama'
     });
 });
 
 router.get('/yomamajoke', function(req, res, next) {
-
 var urlApi = 'http://api.yomomma.info/';
-
 var httpRequest = http.request(urlApi, (httpResponse) => {
 httpResponse.setEncoding('utf8');
 var text = '';
 httpResponse.on('data', (chunk) => {
   text += chunk
-  console.log(`BODY: ${chunk}`);
 });
 httpResponse.on('end', () => {
-  var obj = {
-    joke: JSON.parse(text)
-  }
-  console.log(obj)
-  res.render('joke', obj)
-  console.log('No more data in response.')
+  var joke = JSON.parse(text);
+  // console.log(joke.joke); // This is a string
+  res.render('joke', {joke: joke})
 })
 });
 // write data to request body
 httpRequest.end();
-
-
 });
-
-
 
 router.post('/', function(req, res, next) {
     console.log(req.body);
